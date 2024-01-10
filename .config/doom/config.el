ac4c15dee78ebf3562cd887812acd5e58f365592
ChuMutt <chufilthymutt@gmail.com>
2024-01-10 05:17:35 -0600

    update config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,62 +1,121 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
+(add-hook! org-mode :append
+           #'abbrev-mode)
 
+(setq ispell-alternate-dictionary "/usr/share/dict")
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets. It is optional.
-;; (setq user-full-name "John Doe"
-;;       user-mail-address "john@doe.com")
-(setq user-full-name "Chu the Pup"
-      user-mail-address "chufilthymutt@gmail.com")
+(add-hook! 'text-mode #'flymake-vale-load)
+(add-hook! 'latex-mode-hook #'flymake-vale-load)
+(add-hook! 'org-mode #'flymake-vale-load)
+(add-hook! 'markdown-mode #'flymake-vale-load)
+(add-hook! 'message-mode #'flymake-vale-load)
+(use-package! flymake-vale
+  :config
+  (setq flymake-vale-program "/usr/bin/vale"))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they
-;; accept. For example:
-;;
-;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
-;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
-;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
-;; refresh your font settings. If Emacs still can't find your font, it likely
-;; wasn't installed correctly. Font issues are rarely Doom issues!
+(use-package! palimpsest-mode
+  :hook (prog-mode . palimpsest-mode))
 
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(use-package! vulpea
+  :hook ((org-roam-db-autosync-mode . vulpea-db-autosync-enable)))
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq auth-source-save-behavior nil)
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/nextcloud/documents/org")
+(setq emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
 
-(setq org-agenda-files '("~/nextcloud/documents/org/roam/20221004221829-todo.org"
-                         "~/nextcloud/documents/org/roam/20220823133453-precalculus_algebra.org"
-                         "~/nextcloud/documents/org/roam/20220826102101-chem_1110.org"
-                         "~/nextcloud/documents/org/roam/20220726210346-important_dates.org"
-                         "~/nextcloud/documents/org/roam/20221004222235-notes.org"
-                         "~/nextcloud/documents/org/roam/20221004222230-journal.org"
-                         "~/nextcloud/documents/org/roam/20221004222226-projects.org"
-                         "~/nextcloud/documents/org/roam/20220822103202-engl_1020.org"
-                         "~/nextcloud/documents/org/roam/20221002161620-my_conlang.org"))
+(setq emms-source-file-default-directory "~/Music/")
 
-(setq +org-capture-bookmarks-file "~/nextcloud/documents/org/roam/20221004090130-bookmarks.org")
+(setq dired-backup-overwrite t)
 
-(setq org-roam-directory "~/nextcloud/documents/org/roam")
+(setq delete-by-moving-to-trash t)
+
+(setq warning-suppress-types (append warning-suppress-types '((org-element-cache))))
+
+(add-hook 'org-mode-hook 'org-auto-tangle-mode)
+
+(add-hook 'org-mode-hook 'org-fragtog-mode)
+
+(setq org-todo-keywords
+       '((sequence "TODO(t)" "PROJ(p)" "LOOP(r)" "STRT(s)" "WAIT(w)" "HOLD(h)" "HABIT(H)" "IDEA(i)" "|" "DONE(d)" "KILL(k)")
+         (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
+         (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
+
+  ;;;###autoload
+  (defun unpackaged/org-fix-blank-lines (&optional prefix)
+    "Ensure that blank lines exist between headings and between headings and their contents.
+  With prefix, operate on whole buffer. Ensures that blank lines
+  exist after each headings's drawers."
+    (interactive "P")
+    (org-map-entries (lambda ()
+                       (org-with-wide-buffer
+                        ;; `org-map-entries' narrows the buffer, which prevents us from seeing
+                        ;; newlines before the current heading, so we do this part widened.
+                        (while (not (looking-back "\n\n" nil))
+                          ;; Insert blank lines before heading.
+                          (insert "\n")))
+                       (let ((end (org-entry-end-position)))
+                         ;; Insert blank lines before entry content
+                         (forward-line)
+                         (while (and (org-at-planning-p)
+                                     (< (point) (point-max)))
+                           ;; Skip planning lines
+                           (forward-line))
+                         (while (re-search-forward org-drawer-regexp end t)
+                           ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
+                           ;; for some reason it doesn't work correctly when operating on hidden text.
+                           ;; This works, taken from `org-agenda-get-some-entry-text'.
+                           (re-search-forward "^[ \t]*:END:.*\n?" end t)
+                           (goto-char (match-end 0)))
+                         (unless (or (= (point) (point-max))
+                                     (org-at-heading-p)
+                                     (looking-at-p "\n"))
+                           (insert "\n"))))
+                     t (if prefix
+                           nil
+                         'tree)))
+
+(with-eval-after-load 'org
+  (setq org-directory
+        (concat
+         (getenv "HOME")
+        "/nextcloud/documents/org/")))
+
+(with-eval-after-load 'org
+  (setq +org-capture-bookmarks-file
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/20221004090130-bookmarks.org")))
+
+(with-eval-after-load 'org
+  (setq +org-capture-journal-file
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/20221004222230-journal.org")))
+
+(with-eval-after-load 'org
+  (setq +org-capture-notes-file
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/20221004222235-notes.org")))
+
+(with-eval-after-load 'org
+  (setq +org-capture-projects-file
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/20221004222226-projects.org")))
+
+(with-eval-after-load 'org
+  (setq +org-capture-todo-file
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/20221004221829-todo.org")))
+
+(with-eval-after-load 'org
+  (setq org-roam-directory
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/")))
 
 (setq org-roam-dailies-directory "daily/")
 
@@ -66,36 +125,36 @@
          :target (file+head "%<%Y-%m-%d>.org"
                             "#+title: %<%Y-%m-%d>\n"))))
 
-(setq +org-capture-journal-file "~/nextcloud/documents/org/roam/20221004222230-journal.org")
-
-(setq +org-capture-notes-file "~/nextcloud/documents/org/roam/20221004222235-notes.org")
-
-(setq +org-capture-projects-file "~/nextcloud/documents/org/roam/20221004222226-projects.org")
-
-(setq +org-capture-todo-file "~/nextcloud/documents/org/roam/20221004221829-todo.org")
-
-(setq org-id-locations-file "~/nextcloud/documents/org/.orgids")
-
 (require 'org-roam-protocol)
 
 (require 'org-roam-export)
 
-(setq org-attach-id-dir "~/nextcloud/documents/org/.attach")
+(setq org-id-locations-file
+      (concat
+       (getenv "HOME")
+       "/nextcloud/documents/org/.orgids"))
 
-(setq! org-cite-global-bibliography "~/nextcloud/documents/org/roam/bib.bib")
+(setq org-attach-id-dir
+      (concat
+       (getenv "HOME")
+       "/nextcloud/documents/org/.attach/"))
 
-(setq org-cite-csl-styles-dir "~/nextcloud/documents/org/latex/citeproc-formatters/")
+(setq! org-cite-global-bibliography
+       (list
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/bib.bib")))
 
-(setq citar-bibliography "~/nextcloud/documents/org/roam/bib.bib")
+(setq org-cite-csl-styles-dir
+      (concat
+       (getenv "HOME")
+       "/nextcloud/documents/org/latex/citeproc-formatters/"))
 
-(add-hook 'org-mode-hook 'org-auto-tangle-mode)
-
-(add-hook 'org-mode-hook 'org-fragtog-mode)
-
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "PROJ(p)" "LOOP(r)" "STRT(s)" "WAIT(w)" "HOLD(h)" "HABIT(H)" "IDEA(i)" "|" "DONE(d)" "KILL(k)")
-        (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
-        (sequence "|" "OKAY(o)" "YES(y)" "NO(n)")))
+(setq citar-bibliography
+       (list
+        (concat
+         (getenv "HOME")
+         "/nextcloud/documents/org/roam/bib.bib")))
 
 (with-eval-after-load 'org
   (require 'org-download)
@@ -104,74 +163,134 @@
 (setq org-image-actual-width 500)
 
 (with-eval-after-load 'ox-latex
-  (add-to-list 'org-latex-classes
-               '("org-plain-latex"
-                 "\\documentclass{article}
+(add-to-list 'org-latex-classes
+             '("org-plain-latex"
+               "\\documentclass{article}
            [NO-DEFAULT-PACKAGES]
            [PACKAGES]
            [EXTRA]"
-                 ("\\section{%s}" . "\\section*{%s}")
-                 ("\\subsection{%s}" . "\\subsection*{%s}")
-                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+
+(provide 'org-archive-subtree-hierarchical)
+(require 'org-archive)
+(defun org-archive-subtree-hierarchical--line-content-as-string ()
+  "Returns the content of the current line as a string"
+  (save-excursion
+    (beginning-of-line)
+    (buffer-substring-no-properties
+     (line-beginning-position) (line-end-position))))
+(defun org-archive-subtree-hierarchical--org-child-list ()
+  "This function returns all children of a heading as a list. "
+  (interactive)
+  (save-excursion
+    ;; this only works with org-version > 8.0, since in previous
+    ;; org-mode versions the function (org-outline-level) returns
+    ;; gargabe when the point is not on a heading.
+    (if (= (org-outline-level) 0)
+        (outline-next-visible-heading 1)
+      (org-goto-first-child))
+    (let ((child-list (list (org-archive-subtree-hierarchical--line-content-as-string))))
+      (while (org-goto-sibling)
+        (setq child-list (cons (org-archive-subtree-hierarchical--line-content-as-string) child-list)))
+      child-list)))
+(defun org-archive-subtree-hierarchical--org-struct-subtree ()
+  "This function returns the tree structure in which a subtree belongs as a list."
+  (interactive)
+  (let ((archive-tree nil))
+    (save-excursion
+      (while (org-up-heading-safe)
+        (let ((heading
+               (buffer-substring-no-properties
+                (line-beginning-position) (line-end-position))))
+          (if (eq archive-tree nil)
+              (setq archive-tree (list heading))
+            (setq archive-tree (cons heading archive-tree))))))
+    archive-tree))
+(defun org-archive-subtree-hierarchical ()
+  "This function archives a subtree hierarchical"
+  (interactive)
+  (let ((org-tree (org-archive-subtree-hierarchical--org-struct-subtree))
+        (this-buffer (current-buffer))
+        (file (abbreviate-file-name
+               (or (buffer-file-name (buffer-base-buffer))
+                   (error "No file associated to buffer")))))
+    (save-excursion
+      (setq location org-archive-location
+            afile (car (org-archive--compute-location
+                        (or (org-entry-get nil "ARCHIVE" 'inherit) location)))
+            ;; heading (org-extract-archive-heading location)
+            infile-p (equal file (abbreviate-file-name (or afile ""))))
+      (unless afile
+        (error "Invalid `org-archive-location'"))
+      (if (> (length afile) 0)
+          (setq newfile-p (not (file-exists-p afile))
+                visiting (find-buffer-visiting afile)
+                buffer (or visiting (find-file-noselect afile)))
+        (setq buffer (current-buffer)))
+      (unless buffer
+        (error "Cannot access file \"%s\"" afile))
+      (org-cut-subtree)
+      (set-buffer buffer)
+      (org-mode)
+      (goto-char (point-min))
+      (while (not (equal org-tree nil))
+        (let ((child-list (org-archive-subtree-hierarchical--org-child-list)))
+          (if (member (car org-tree) child-list)
+              (progn
+                (search-forward (car org-tree) nil t)
+                (setq org-tree (cdr org-tree)))
+            (progn
+              (goto-char (point-max))
+              (newline)
+              (org-insert-struct org-tree)
+              (setq org-tree nil)))))
+      (newline)
+      (org-yank)
+      (when (not (eq this-buffer buffer))
+        (save-buffer))
+      (message "Subtree archived %s"
+               (concat "in file: " (abbreviate-file-name afile))))))
+(defun org-insert-struct (struct)
+  "TODO"
+  (interactive)
+  (when struct
+    (insert (car struct))
+    (newline)
+    (org-insert-struct (cdr struct))))
+(defun org-archive-subtree ()
+  (org-archive-subtree-hierarchical))
 
 (setq org-archive-default-command 'org-archive-subtree-hierarchical)
 
+(with-eval-after-load 'org
+  (setq org-agenda-files '("~/nextcloud/documents/org/roam/20221004221829-todo.org"
+                           "~/nextcloud/documents/org/roam/20220823133453-precalculus_algebra.org"
+                           "~/nextcloud/documents/org/roam/20220826102101-chem_1110.org"
+                           "~/nextcloud/documents/org/roam/20220726210346-important_dates.org"
+                           "~/nextcloud/documents/org/roam/20221004222235-notes.org"
+                           "~/nextcloud/documents/org/roam/20221004222230-journal.org"
+                           "~/nextcloud/documents/org/roam/20221004222226-projects.org"
+                           "~/nextcloud/documents/org/roam/20220822103202-engl_1020.org"
+                           "~/nextcloud/documents/org/roam/20221002161620-my_conlang.org")))
 
-;; Whenever you reconfigure a package, make sure to wrap your config in an
-;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their
-;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-;;
-;; Here are some additional functions/macros that will help you configure Doom.
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
-;; etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-(add-load-path! "~/.config/doom/elisp")
+(require 'org-habit)
 
-(require 'org-archive-subtree-hierarchical)
-
-(setq erc-server "localhost"
-      erc-nick "chuthepup"
-      erc-user-full-name "Chu the Pup")
-
-(setq delete-by-moving-to-trash t)
-
-(setq dired-backup-overwrite t)
+(setq doom-font (font-spec :size 12))
 
 (setq image-use-external-converter t)
 
 (require 'random-splash-image)
 
-(setq random-splash-image-dir "~/.local/share/random-splash-image-dir/chosen-splash-images/src/")
+(setq random-splash-image-dir
+      (concat
+       (getenv "HOME") "/.local/share/random-splash-image-dir/chosen-splash-images/src/"))
 
 (with-eval-after-load 'random-splash-image
   (random-splash-image-set))
-
-(setq skeletor-project-directory "~/nextcloud/projects/")
 
 (when (and (not (executable-find "fd"))
            (executable-find "rg"))
@@ -186,25 +305,43 @@
 
 (elcord-mode)
 
+(setq ange-ftp-netrc-filename "~/.authinfo.gpg")
+
 (achievements-mode)
 
-(setq browse-url-firefox-arguments "-P chu")
-(setq browse-url-firefox-new-window-is-tab t)
+(defun get-number-at-point ()
+  (interactive)
+  (skip-chars-backward "0123456789.-")
+  (or (looking-at "[0123456789.-]+")
+      (error "No number at point"))
+  (string-to-number (match-string 0)))
 
-(use-package! literate-calc-mode :defer t)
+(defun round-number-at-point-to-decimals (decimal-count)
+  (interactive "NDecimal count: ")
+  (let ((mult (expt 10 decimal-count)))
+    (replace-match (number-to-string
+              (/
+               (fround
+                (*
+                 mult
+                 (get-number-at-point)))
+                mult)))))
+
+;; (setq browse-url-firefox-program "/usr/bin/firefox")
+;; (setq browse-url-firefox-arguments "-no-remote")
+;; (setq browse-url-firefox-new-window-is-tab t)
+;; (setq browse-url-generic-program "/usr/bin/firefox")
+;; (setq browse-url-browser-function 'browse-url-generic)
+
+(use-package! literate-calc-mode
+  :defer t)
+
+(defun +lisp/find-file-in-quicklisp ()
+  "Find a file belonging to a library downloaded by Quicklisp."
+  (interactive)
+  (doom-project-find-file "~/.local/share/roswell/lisp/quicklisp/dists"))
 
 (load! (expand-file-name "~/.local/share/roswell/helper.el"))
-
-(use-package! slime
-  :defer t ; don't load the package immediately
-  :init ; runs this immediately
-  (setq inferior-lisp-program "sbcl")
-  :config ; runs this when slime loads
-  (set-repl-handler! 'lisp-mode #'sly-mrepl)
-  (set-eval-handler! 'lisp-mode #'sly-eval-region)
-  (set-lookup-handlers! 'lisp-mode
-    :definition #'sly-edit-definition
-    :documentation #'sly-describe-symbol)
-  (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode))
+(setq inferior-lisp-program "ros -Q run")
 
 (require 'common-lisp-snippets)
