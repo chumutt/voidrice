ff40cc5e709a50530310ea0eca32242e3cf8ba4f
ChuMutt <chufilthymutt@gmail.com>
2024-01-22 12:16:45 -0600

    updates

--- .config/firefox/larbs.js
+++ .config/firefox/larbs.js
@@ -8,8 +8,8 @@ user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
 // Do not suggest web history in the URL bar:
 user_pref("browser.urlbar.suggest.history", false);
 
-// Do not prefil forms:
-user_pref("signon.prefillForms", false);
+// Prefill forms:
+user_pref("signon.prefillForms", true);
 
 // Do not autocomplete in the URL bar:
 user_pref("browser.urlbar.autoFill", false);
@@ -21,9 +21,9 @@ user_pref("keyword.enabled", true);
 user_pref("dom.security.https_only_mode", false);
 
 // Keep cookies until expiration or user deletion:
-user_pref("network.cookie.lifetimePolicy", 0);
+user_pref("network.cookie.lifetimePolicy", 1);
 
-user_pref("dom.webnotifications.serviceworker.enabled", false);
+user_pref("dom.webnotifications.serviceworker.enabled", true);
 
 // Disable push notifications:
 user_pref("dom.push.enabled", false);
@@ -34,17 +34,28 @@ user_pref("extensions.pocket.enabled", false);
 // Don't autodelete cookies on shutdown:
 user_pref("privacy.clearOnShutdown.cookies", false);
 
+// Don't autodelete history on shutdown:
+user_pref("privacy.clearOnShutdown.history", false);
+
+user_pref("privacy.clearOnShutdown.downloads", false);
+
+user_pref("privacy.clearOnShutdown.cache", false);
+
+user_pref("privacy.clearOnShutdown.sessions", false);
+user_pref("privacy.clearOnShutdown.formdata", false);
+user_pref("privacy.clearOnShutdown.offlineApps", false);
+user_pref("browser.sessionstore.resume_from_crash", false);
+user_pref("webgl.disabled", false);
+
 // Enable custom userChrome.js:
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// This could otherwise cause some issues on bank logins and other annoying sites:
-user_pref("network.http.referer.XOriginPolicy", 0);
-
-// Disable Firefox sync and its menu entries
-user_pref("identity.fxaccounts.enabled", false);
+// Enable Firefox sync and its menu entries
+user_pref("identity.fxaccounts.enabled", true);
 
 // Fix the issue where right mouse button instantly clicks
 user_pref("ui.context_menus.after_mouseup", true);
 
-// Disable extension signing requirement
-// user_pref("xpinstall.signatures.required", false);
+// Enable Autoscroll safely
+user_pref("middlemouse.paste", false);
+user_pref("general.autoScroll", true);
